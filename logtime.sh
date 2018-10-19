3ec069a5b33f6cae70df2b3300a60869c657d804
JourneyVN <ngoducquyet@yahoo.com>
2018-10-19 13:32:00 +0700

    Update logtime.sh

--- logtime.sh
+++ logtime.sh
@@ -1,6 +1,6 @@
 #/bin/date  --rfc-3339=seconds >> timeresult
 #/bin/echo -n " Thoi gian log time job1"  >> timeresult
-
+echo $branch
 /bin/date +"Thoi gian log time job $JOB_NAME vao luc: %T_%d:%m:%y" >> timeresult
 
 
